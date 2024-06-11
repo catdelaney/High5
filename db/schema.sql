c1ae723f77729ef6242f9b456a57860ce5067fe4
catdelaney <delan@DESKTOP-PTA0O78.(none)>
2024-06-11 05:01:23 +0200

    create table bug fixes

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE employeeInfo (
     employee_id SERIAL PRIMARY KEY,
     fName VARCHAR(50),
     lName VARCHAR(50),
-    department VARCHAR(50),
+    department VARCHAR(50)
 );
 
 CREATE TABLE performance (
@@ -16,11 +16,11 @@ CREATE TABLE performance (
     review_date DATE,
     revenue_generated INT CHECK (revenue_generated BETWEEN 1 AND 5),
     work_quality INT CHECK (work_quality BETWEEN 1 AND 5),
-    new_existing_business INT CHECK (new_existing_business BETWEEN 1 AND 5),
+    new_existing_business INT CHECK (new_existing_business BETWEEN 1 AND 5)
 );
 
 CREATE TABLE recognition (
     recognition_id SERIAL PRIMARY KEY,
     employee_id INT REFERENCES employeeInfo(employee_id),
-    recognition_date DATE,
-)
+    recognition_date DATE
+);
