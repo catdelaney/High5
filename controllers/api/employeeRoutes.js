c914a844e7bc9276a75ac9191d5eb9434f70f3b3
catdelaney <delan@DESKTOP-PTA0O78.(none)>
2024-06-12 13:03:00 +0200

    change file names

--- controllers/api/employeeRoutes.js
+++ controllers/api/employeeRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-router.post('/', async (req, res) => {
+router.post('/employees', async (req, res) => {
   try {
     const userData = await User.create(req.body);
 
