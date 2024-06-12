9bf00fd632ebccf38721a10adf6241e922c377e2
catdelaney <delan@DESKTOP-PTA0O78.(none)>
2024-06-12 14:06:45 +0200

    create and get api

--- controllers/api/performanceRoutes.js
+++ controllers/api/performanceRoutes.js
@@ -1,19 +1,48 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Employee, Performance } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+
+//Create Performance Reviews
 router.post('/', withAuth, async (req, res) => {
+  const { employee_id, review_date, revenue_generated, work_quality, new_existing_business } = req.body;
   try {
-    const newProject = await Project.create({
-      ...req.body,
-      user_id: req.session.user_id,
+    const performance = await Performance.create({
+      employee_id,
+      review_date,
+      revenue_generated,
+      work_quality,
+      new_existing_business,
+      //is the next line needed?
+      //user_id: req.session.user_id,
     });
 
-    res.status(200).json(newProject);
+    res.status(200).json(performance);
   } catch (err) {
+    console.error(err.message);
     res.status(400).json(err);
   }
 });
+
+//Get Performance Reviews
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const performances = await Performance.findAll({
+      include: [{
+        model: Employee,
+        attributes: ['fName', 'lName', 'department']
+      }]
+    });
+    res.status(200).json(performances);
+  } catch (err) {
+    console.error(err.message);
+    res.status(400).json(err);
+  }
+  });
+
+
+
+
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
