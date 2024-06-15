d9f9701fa1eba2913926e0f62ea06b42ea96d86a
Michelle Bell <michbesch0@outlook.com>
2024-06-15 10:16:15 -0400

    fixed bug

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const employeeRoutes = require('./employeeRoutes');
-// const performanceRoutes = require('./performanceRoutes');
-// const recognitionRoutes = require('./recognitionRoutes');
+const performanceRoutes = require('./performanceRoutes');
+const recognitionRoutes = require('./recognitionRoutes');
 
 router.use('/employees', employeeRoutes);
-// router.use('/performances', performanceRoutes);
-// router.use('/recognition', recognitionRoutes);
+router.use('/performances', performanceRoutes);
+router.use('/recognition', recognitionRoutes);
 
 module.exports = router;
