3aac450442b4f2ba1333ec0f9ba70161bca32ea1
Natalie Clinton <natalie.clinton@hotmail.com>
2024-06-17 17:31:58 -0400

    fixed nav, signup, added json data, added text to analytics

--- public/js/login.js
+++ public/js/login.js
@@ -25,14 +25,16 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#name-signup').value.trim();
+  const firstName = document.querySelector('#firstName-signup').value.trim();
+  const lastName = document.querySelector('#lastName-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
+  const department = document.querySelector('#department-signup').value.trim();
 
-  if (name && email && password) {
+  if (firstName && lastName && email && password && department) {
     const response = await fetch('/api/employees', {
       method: 'POST',
-      body: JSON.stringify({ name, email, password }),
+      body: JSON.stringify({ firstName, lastName, email, password, department }),
       headers: { 'Content-Type': 'application/json' },
     });
 
